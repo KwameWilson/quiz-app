f5d5e766a278c6f0f20b63ea8351710b3aaffcaa
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-02-15 16:28:13 +0000

    Refactored darkMode to include arrow functions called in the eventListener method

--- script/darkMode.js
+++ script/darkMode.js
@@ -2,13 +2,22 @@ const modeBody = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 const categoryDarkMode = document.querySelectorAll('.grid-right > div')
 
+
+const darkMode = () => {
+    modeBody.classList.remove('light-mode')
+    modeBody.classList.add('dark-mode')
+}
+
+const lightMode = () => {
+    modeBody.classList.remove('dark-mode')
+    modeBody.classList.add('light-mode')
+}
+
 toggle.addEventListener('click', () => {
     // console.log(e.target);
     if (modeBody.classList.contains('light-mode')) {
-        modeBody.classList.remove('light-mode')
-        modeBody.classList.add('dark-mode')
+        darkMode()
     } else {
-        modeBody.classList.remove('dark-mode')
-        modeBody.classList.add('light-mode')
+        lightMode()
     }
 })
