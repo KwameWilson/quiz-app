573aa604d47dfae6576451ed71f621a1b45bcf7b
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-02-15 14:08:28 +0000

    Making changes to the light and dark mode classes

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,8 +1,9 @@
 const darkModeBody = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
+const categoryDarkMode = document.querySelectorAll('.grid-right > div')
 
-toggle.addEventListener('click', (e) => {
-    console.log(e.target);
+toggle.addEventListener('click', () => {
+    // console.log(e.target);
     if (darkModeBody.classList.contains('dark-mode')) {
         darkModeBody.classList.remove('dark-mode')
     } else {
