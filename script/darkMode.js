1ecdf5e1a6bed1cac713e40a9ef60e4f58ba988e
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-02-15 14:19:25 +0000

    Finished the dark mode and light modes

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,12 +1,14 @@
-const darkModeBody = document.querySelector('body');
+const modeBody = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
 const categoryDarkMode = document.querySelectorAll('.grid-right > div')
 
 toggle.addEventListener('click', () => {
     // console.log(e.target);
-    if (darkModeBody.classList.contains('dark-mode')) {
-        darkModeBody.classList.remove('dark-mode')
+    if (modeBody.classList.contains('light-mode')) {
+        modeBody.classList.remove('light-mode')
+        modeBody.classList.add('dark-mode')
     } else {
-        darkModeBody.classList.add('dark-mode')
+        modeBody.classList.remove('dark-mode')
+        modeBody.classList.add('light-mode')
     }
 })
